4dce47332b1ac5a7139cea21c3b7fa7ab424d5f5
Daniel <dansen95@mail.ru>
2021-02-09 15:19:40 +0300

    homework finished

--- homework.py
+++ homework.py
@@ -18,7 +18,7 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 API_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-
+print(CHAT_ID)
 
 def parse_homework_status(homework):
     try:
